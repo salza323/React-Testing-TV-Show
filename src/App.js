7b7edd795150dd7d61a1ea60bcee2047bd9456d0
salza323 <sal.zamora480@gmail.com>
2020-10-19 20:47:19 -0700

    mvp

--- src/App.js
+++ src/App.js
@@ -1,34 +1,47 @@
-import React, { useState, useEffect } from "react";
-import axios from "axios";
-import Dropdown from "react-dropdown";
-import parse from "html-react-parser";
+import React, { useState, useEffect } from 'react';
+import axios from 'axios';
+import Dropdown from 'react-dropdown';
+import parse from 'html-react-parser';
 
-import { formatSeasons } from "./utils/formatSeasons";
+import { formatSeasons } from './utils/formatSeasons';
+import fetchShow from './api/fetchShow';
 
-import Episodes from "./components/Episodes";
-import "./styles.css";
+import Episodes from './components/Episodes';
+import './styles.css';
 
 export default function App() {
   const [show, setShow] = useState(null);
   const [seasons, setSeasons] = useState([]);
-  const [selectedSeason, setSelectedSeason] = useState("");
+  const [selectedSeason, setSelectedSeason] = useState('');
   const episodes = seasons[selectedSeason] || [];
 
+  // useEffect(() => {
+  //   const fetchShow = () => {
+  //     axios
+  //       .get(
+  //         "https://api.tvmaze.com/singlesearch/shows?q=stranger-things&embed=episodes"
+  //       )
+  //       .then(res => {
+  //         setShow(res.data);
+  //         setSeasons(formatSeasons(res.data._embedded.episodes));
+  //       });
+  //   };
+  //   fetchShow();
+  // }, []);
+
   useEffect(() => {
-    const fetchShow = () => {
-      axios
-        .get(
-          "https://api.tvmaze.com/singlesearch/shows?q=stranger-things&embed=episodes"
-        )
-        .then(res => {
-          setShow(res.data);
-          setSeasons(formatSeasons(res.data._embedded.episodes));
-        });
-    };
-    fetchShow();
+    console.log('1');
+    fetchShow().then((res) => {
+      console.log('4');
+
+      setShow(res.data);
+      setSeasons(formatSeasons(res.data._embedded.episodes));
+      console.log('5');
+    });
+    console.log('3');
   }, []);
 
-  const handleSelect = e => {
+  const handleSelect = (e) => {
     setSelectedSeason(e.value);
   };
 
@@ -37,15 +50,15 @@ export default function App() {
   }
 
   return (
-    <div className="App">
-      <img className="poster-img" src={show.image.original} alt={show.name} />
+    <div className='App'>
+      <img className='poster-img' src={show.image.original} alt={show.name} />
       <h1>{show.name}</h1>
       {parse(show.summary)}
       <Dropdown
         options={Object.keys(seasons)}
         onChange={handleSelect}
-        value={selectedSeason || "Select a season"}
-        placeholder="Select an option"
+        value={selectedSeason || 'Select a season'}
+        placeholder='Select an option'
       />
       <Episodes episodes={episodes} />
     </div>
